f946fdedea6843be4cc5e81adb6ddd8254efd9dd
Harsh Jadon <harshjadon9098@gmail.com>
2025-06-02 13:33:02 +0530

    updated homepage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-    <Router>
+    <Router basename="/quizwhiz">
       <Navbar />
 
       <Routes>
